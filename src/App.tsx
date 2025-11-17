504edb8396143e9c2172d4bb99647f7091c3b2cd
Bridges22 <bridgesmwashighadi2@gmail.com>
2025-11-17 08:10:26 +0300

    Update project structure to build from root directory

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import { BrowserRouter } from "react-router-dom";
+import { AppRoutes } from "./router";
+import { I18nextProvider } from "react-i18next";
+import i18n from "./i18n";
+import { useEffect } from "react";
+import { initAllAnimations } from "./utils/scrollAnimations";
+import "./utils/animations.css";
+
+
+function App() {
+  useEffect(() => {
+    initAllAnimations();
+  }, []);
+
+  return (
+    <I18nextProvider i18n={i18n}>
+      <BrowserRouter basename={__BASE_PATH__}>
+        <AppRoutes />
+      </BrowserRouter>
+    </I18nextProvider>
+  );
+}
+
+export default App;
